ea9eb4d72f087760eb1c0b6958247fe84b5d4880
Sachin Karthik <sachinkarthikbs@gmail.com>
2024-09-09 14:50:47 +0530

    Add Task Button

--- todolist/src/components/Nav-Button.js
+++ todolist/src/components/Nav-Button.js
@@ -1,11 +1,33 @@
 import * as React from 'react';
 import Stack from '@mui/material/Stack';
 import Button from '@mui/material/Button';
+import { useState } from 'react';
+import { Dialog, DialogActions, DialogContent, DialogTitle } from '@mui/material';
 
-export default function BasicButtons() {
+const BasicButtons = () => {
+    const [open,setOpen] = useState(false);
+    const openpopup = ()=>{
+      setOpen(true);
+    }
+    const closepopup = () =>{
+      setOpen(false);
+    }
+  
   return (
-    <Stack spacing={2} direction="row">
-      <Button variant="contained">Add Task</Button>
+    <>
+    <Stack spacing={3} direction="row">
+      <Button onClick={openpopup} variant="contained">Add Task</Button>
     </Stack>
+    <Dialog open={open} onClose={closepopup} fullWidth maxWidth="sm" >
+      <DialogTitle>Todo</DialogTitle>
+      <DialogContent>Any</DialogContent>
+      <DialogActions>
+      <Stack spacing={6} direction="row">
+      <Button onClick={closepopup} variant="contained">Close</Button>
+      </Stack>
+      </DialogActions>
+    </Dialog>
+    </>
   );
 }
+export default BasicButtons;
